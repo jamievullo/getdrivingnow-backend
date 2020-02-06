a7b3f3b004d433e547bad59a01fafb09426afe1c
jamievullo <jvullo@protonmail.com>
2020-02-06 16:41:48 -0500

    Added validations to driver model and setup for oauth

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,26 @@
 class Driver < ApplicationRecord
+
+    has_secure_password
+
+    validates :password, presence: true, if: :setting_password?
+    validates :email, presence: true, length: { minimum: 5 }
+    validates :email, uniqueness: { case_sensitive: false }
+    validate :email_must_be_valid
+
+    def self.find_or_create_by_omniauth(auth_hash)        
+        account = self.find_or_create_by(:uid => auth_hash[:uid]) do |a|
+            a.username = auth_hash[:info][:name]
+            a.email = auth_hash[:info][:email]
+            a.password = SecureRandom.hex 
+        end
+    end
+    
+    def email_must_be_valid
+        errors.add(:email, "must be a valid email address") unless email.include?(".") and email.include?("@")
+    end
+    
+    def setting_password?
+        password || password_confirmation
+    end
+
 end
